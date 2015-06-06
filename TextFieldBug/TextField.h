4abd72359a6979c49340359d9107d0328a192deb
Christian Schnorr <christian.schnorr@me.com>
2015-06-06 20:13:36 +0200

    Fix vertical alignment in `UITextField`s

---
+++ TextFieldBug/TextField.h
@@ -0,0 +1,13 @@
+//
+//  TextField.h
+//  TextFieldBug
+//
+//  Created by Christian Schnorr on 6/6/15.
+//  Copyright (c) 2015 Diogo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextField : UITextField
+
+@end
---
+++ TextFieldBug/TextField.m
@@ -0,0 +1,28 @@
+//
+//  TextField.m
+//  TextFieldBug
+//
+//  Created by Christian Schnorr on 6/6/15.
+//  Copyright (c) 2015 Diogo. All rights reserved.
+//
+
+#import "TextField.h"
+
+@implementation TextField
+
+- (CGRect)editingRectForBounds:(CGRect)bounds
+{
+    if (UIDevice.currentDevice.systemVersion.integerValue != 8) return [self textRectForBounds:bounds];
+
+    CGFloat const scale = UIScreen.mainScreen.scale;
+    CGFloat const preferred = [self.text sizeWithAttributes:self.defaultTextAttributes].height;
+    CGFloat const delta = ceil(preferred) - preferred;
+    CGFloat const adjustment = floor(delta * scale) / scale;
+
+    CGRect const textRect = [self textRectForBounds:bounds];
+    CGRect const editingRect = CGRectOffset(textRect, 0.0, adjustment);
+
+    return editingRect;
+}
+
+@end
